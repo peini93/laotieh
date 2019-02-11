dfae2718c0c41f1841d399a894582474aaca9bf8
MEI93-PC\Administrator <mei319319319@gmail.com>
2019-02-11 18:32:40 +0800

    新增

---
+++ js/page.js
@@ -0,0 +1,12 @@
+$(function(){
+
+		// $('body').jpreLoader({
+		// 	splashID: "#jSplash",
+		// 	loaderVPos: '50%',
+		// 	splashVPos: '50%',
+		// 	autoClose: true
+		// }, function() { //callback function
+		// });
+
+
+});
