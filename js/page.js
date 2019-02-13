8ba0ab26b314c503c935ca75f83887ee3b5d07ab
MEI93-PC\Administrator <mei319319319@gmail.com>
2019-02-13 18:52:41 +0800

    新增修改

--- js/page.js
+++ js/page.js
@@ -1,12 +1,15 @@
 $(function(){
 
-		// $('body').jpreLoader({
-		// 	splashID: "#jSplash",
-		// 	loaderVPos: '50%',
-		// 	splashVPos: '50%',
-		// 	autoClose: true
-		// }, function() { //callback function
-		// });
-
+	$( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "" + ui.values[ 0 ] + "---" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "" + $( "#slider-range" ).slider( "values", 0 ) +
+      "---" + $( "#slider-range" ).slider( "values", 1 ) );
 
 });
