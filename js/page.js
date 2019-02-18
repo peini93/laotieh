8b817db3b7281adcc1eb267669c337ab83aaaf24
MEI93-PC\Administrator <mei319319319@gmail.com>
2019-02-18 12:40:06 +0800

    修改新增

--- js/page.js
+++ js/page.js
@@ -1,15 +1,26 @@
 $(function(){
 
-	$( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "" + ui.values[ 0 ] + "---" + ui.values[ 1 ] );
-      }
+	// $( "#slider-range" ).slider({
+ //      range: true,
+ //      min: 0,
+ //      max: 500,
+ //      values: [ 75, 300 ],
+ //      slide: function( event, ui ) {
+ //        $( "#amount" ).val( "" + ui.values[ 0 ] + "---" + ui.values[ 1 ] );
+ //      }
+ //    });
+
+ //    $( "#amount" ).val( "" + $( "#slider-range" ).slider( "values", 0 ) +
+ //      "---" + $( "#slider-range" ).slider( "values", 1 ) );
+
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 1000,
+        grid: true
     });
-    $( "#amount" ).val( "" + $( "#slider-range" ).slider( "values", 0 ) +
-      "---" + $( "#slider-range" ).slider( "values", 1 ) );
 
 });
